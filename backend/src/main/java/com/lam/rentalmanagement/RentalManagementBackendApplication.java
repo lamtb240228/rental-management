a21ab19269828e3f98f976f7d7927e365e6bb8b0
lamtr <lamtbts02316@gmail.com>
2026-07-06 16:06:04 +0700

    chore: add Docker PostgreSQL setup

---
+++ backend/src/main/java/com/lam/rentalmanagement/RentalManagementBackendApplication.java
@@ -0,0 +1,15 @@
+package com.lam.rentalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                RentalManagementBackendApplication.class,
+                args
+        );
+    }
+}
